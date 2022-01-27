0577db81198c7e2c2720146f85952e0f22810093
xiaoyao91 <xiaoyao91@jd.com>
2022-01-27 19:00:48 +0800

    nothing

--- src/main/java/com/jx/dynamiclog/web/HelloController.java
+++ src/main/java/com/jx/dynamiclog/web/HelloController.java
@@ -1,7 +1,6 @@
 package com.jx.dynamiclog.web;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.logging.log4j.spi.ThreadContextMapFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,7 +22,7 @@ public class HelloController {
         map.put("code","200");
         map.put("message","hello");
 
-        ThreadContextMapFactory.createThreadContextMap().put("User1", "111");
+//        ThreadContextMapFactory.createThreadContextMap().put("User1", "111");
 
         log.info("hi >>>>>>>>>>>>>");
         return map;
